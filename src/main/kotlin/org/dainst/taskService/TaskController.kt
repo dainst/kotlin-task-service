a4dc98374521e968468e0d73319fb889a9d463be
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2018-03-29 12:09:17 +0200

    implement simple message sending with rabbitmq

--- src/main/kotlin/org/dainst/taskService/TaskController.kt
+++ src/main/kotlin/org/dainst/taskService/TaskController.kt
@@ -1,15 +1,44 @@
 package org.dainst.taskService
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import com.rabbitmq.client.ConnectionFactory
+import java.nio.charset.Charset
+
 
 @RestController
+@RequestMapping("/task")
 class TaskController {
 
-    @GetMapping("/task/{id}")
-    fun task(@PathVariable id: String,
+    val QUEUE_NAME = "tasks";
+
+    @PostMapping("/hello")
+    fun hello(): String {
+        val factory = ConnectionFactory()
+        factory.host = "localhost"
+        factory.username = "user"
+        factory.password = "password"
+        factory.virtualHost = "task_queue"
+        val connection = factory.newConnection()
+        val channel = connection.createChannel()
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null)
+        val message = "Hello World"
+        channel.basicPublish(
+                "",
+                QUEUE_NAME,
+                null,
+                message.toByteArray(Charset.forName("UTF-8"))
+        )
+        println(" [x] Sent '$message'")
+
+        channel.close()
+        connection.close()
+
+        return message
+    }
+
+    @GetMapping("/status/{id}")
+    fun status(@PathVariable id: String,
              @RequestParam(value = "name", defaultValue = "DefaultTask") name: String
     ) = Task(id, name)
 
