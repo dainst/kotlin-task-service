71e10308c0aa7a71bdcf9d410cd123b949bbcd09
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2018-04-03 16:45:18 +0200

    vary wait duration in FakeTask
    This is useful for testing task distribution

--- worker/src/main/kotlin/org/dainst/tasks/worker/FakeTask.kt
+++ worker/src/main/kotlin/org/dainst/tasks/worker/FakeTask.kt
@@ -1,8 +1,12 @@
 package org.dainst.tasks.worker
 
+import java.util.*
+
 class FakeTask: RunnableTask {
 
     override fun run() {
-        Thread.sleep(5000L)
+        val random = Random().nextInt(9000) + 1000
+        println(" [x] Waiting for ${random}ms ...")
+        Thread.sleep(random.toLong())
     }
 }
